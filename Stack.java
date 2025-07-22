969bf4fd388a2f4659a7769b4961ed5f8baba048
jjoySubash <164199482+jjoySubash@users.noreply.github.com>
2025-07-22 18:59:35 +0530

    Stack.java

--- Stack.java
+++ Stack.java
@@ -1,37 +1,74 @@
-
 import java.util.*;
 
-public class Stack {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String a = sc.next();
-        char[] stack = new char[a.length()];
-        char[] b = a.toCharArray();
-        int top = -1;
-
-        for (int i = 0; i < a.length(); i++) {
-            if (b[i] == '(' || b[i] == '{' || b[i] == '[') {
-                stack[++top] = b[i];
-            } else if (b[i] == ')' || b[i] == '}' || b[i] == ']') {
-                if (top == -1) {
-                    System.out.println("not balanced");
-                    return;
-                }
-                char last = stack[top];
-                if ((last == '(' && b[i] != ')') ||
-                    (last == '[' && b[i] != ']') ||
-                    (last == '{' && b[i] != '}')) {
-                    System.out.println("not balanced");
-                    return;
-                }
-                top--;
+public class Main
+{
+    static class Stack{
+        int a[];
+        int top = -1, cap;
+        
+        Stack(int size){
+            a = new int[size];
+            cap = size;
+        }
+        
+        void push(int n){
+            
+            if(top == cap -1){
+                System.out.println("FuLL");
+                return;
+            }
+            
+            a[++top] = n;
+        }
+        
+        void pop(){
+            
+            if(top == -1){
+                System.out.println("Empty");
+                return;
+            }
+            
+            top--;
+        }
+        
+        void play(){
+            for(int i=0; i<=top; i++){
+                System.out.print(a[i]+" --> ");
             }
         }
-
-        if (top == -1) {
-            System.out.println("balanced");
-        } else {
-            System.out.println("not balanced");
-        }
     }
+	public static void main(String[] args) {
+		Scanner sc = new Scanner (System.in);
+		
+		System.out.print("Enter the Size of the Stack: ");
+		int s = sc.nextInt();
+		Stack tk = new Stack(s);
+		
+		while(true){
+		    System.out.print("\n1.push\n2.pop\n3.Display\n4.exit\nEnter Command: ");
+		    int op = sc.nextInt();
+		    
+		    switch (op){
+		        case 1:
+		            System.out.print("Enter value to push: ");
+		            int p = sc.nextInt();
+		            tk.push(p);
+		            break;
+		      case 2:
+		          tk.pop();
+		          break;
+		      case 3:
+		          tk.play();
+		          break;
+		      case 4:
+		          return;
+		      default:
+		          System.out.println("Invaid Try AGAIN!!!!");
+		          break;
+		            
+		    }
+		    
+		    
+		}
+	}
 }
